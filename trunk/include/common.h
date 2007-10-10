135b7042833c9684cac7e84932fc828c767a4a60
php.forge <php.forge@4dacca91-f93b-0410-bd3a-4f99cd15f8bd>
2007-10-10 18:45:52 +0000

    Plus de structures de donnees

--- trunk/include/common.h
+++ trunk/include/common.h
@@ -9,4 +9,16 @@ typedef struct {
 	int width, height;
 } Size2D;
 
+typedef struct {
+	float x, y, z;
+} Vector3Df;
+
+typedef struct {
+	float r, g, b;
+} Color3f;
+
+typedef struct {
+	float r, g, b, a;
+} Color4f;
+
 #endif
