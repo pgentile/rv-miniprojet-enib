cc09c7cee796f35ff56fd6c5d6773d715fcf920d
php.forge <php.forge@4dacca91-f93b-0410-bd3a-4f99cd15f8bd>
2007-10-22 10:14:09 +0000

    Ajout du code de l'arbre

--- trunk/src/test.cpp
+++ trunk/src/test.cpp
@@ -1,14 +1,14 @@
 #include "test-window.h"
 #include "test-scene.h"
-#include "barney.h"
-#include "soucoupe.h"
+#include "tree.h"
 
 int main(int argc, char** argv)
 {
 	TestWindow window;
 	TestScene scene;
-	Barney barney;
-	scene.addChild(&barney);
+	Tree tree;
+	
+	scene.addChild(&tree);
 	window.setScene(&scene);
 	Window::create(argc, argv, window);
 	return 0;
--- trunk/src/tree.cpp
+++ trunk/src/tree.cpp
@@ -1,28 +1,38 @@
-#include "barney.h"
+#include "tree.h"
 #include "png-loader.h"
 #include <GL/glut.h>
 
-void Barney::animate(int timerInterval)
+#include <iostream>
+
+void Tree::animate(int)
 {
-	_angle += 2.0;
-	if (_angle > 360.0) {
-		_angle -= 360.0;
-	}
+	// Ne rien faire
 }
 
-void Barney::render(void)
+void Tree::render(void)
 {
-	unsigned int texture = PngLoader::load("barney.png");
+	unsigned int texture = PngLoader::load("tree.png");
 
-	glRotatef(_angle, 0.0, 1.0, 0.0);
-	
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_ALPHA_TEST);
-	glAlphaFunc(GL_GREATER, 0.9);
-
+	glAlphaFunc(GL_GREATER, 0.8);
 	glEnable(GL_TEXTURE_2D);
 	glBindTexture(GL_TEXTURE_2D, texture);
+	
+	for (int i = 0; i < 2; i++) {
+		glPushMatrix();
+		glRotatef((180.0 *  i) / 2, 0.0, 1.0, 0.0);
+		_renderBillboard();
+		glPopMatrix();
+	}
 
+	glDisable(GL_TEXTURE_2D);
+	glDisable(GL_ALPHA_TEST);
+	glDisable(GL_DEPTH_TEST);
+}
+
+void Tree::_renderBillboard(void)
+{
 	glBegin(GL_POLYGON);
 	glColor4f(1.0, 1.0, 1.0, 1.0);
 	glTexCoord2f(0.0, 1.0); glVertex3f(-1.0, 1.0, 0.0);
@@ -30,14 +40,4 @@ void Barney::render(void)
 	glTexCoord2f(1.0, 0.0); glVertex3f(1.0, -1.0, 0.0);
 	glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0, 0.0);
 	glEnd();
-
-	glDisable(GL_TEXTURE_2D);
-	
-	glDisable(GL_ALPHA_TEST);
-	glDisable(GL_DEPTH_TEST);
-}
-
-void Barney::_init(void)
-{
-	_angle = 0.0;
 }
