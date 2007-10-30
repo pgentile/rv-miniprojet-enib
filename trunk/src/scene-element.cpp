f67c9737485b9b0ecd4777d73e9aec77fbc91764
php.forge <php.forge@4dacca91-f93b-0410-bd3a-4f99cd15f8bd>
2007-10-30 10:21:08 +0000

    Changements sur la struture de donnees, donc nettoyage du dépôt avant

--- trunk/src/scene-element.cpp
+++
@@ -1,5 +0,0 @@
-#include "scene-element.h"
-
-void SceneElement::init(void)
-{
-}
--- trunk/src/test-scene.cpp
+++
@@ -1,7 +0,0 @@
-#include "test-scene.h"
-#include <GL/glut.h>
-
-void TestScene::_setCamera(void)
-{
-	gluLookAt(0.0, 0.0, 7.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-}
